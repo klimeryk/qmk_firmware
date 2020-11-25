c951f680cd03d1cd44f997b684d6b4703735b4f6
Igor Klimer <github@gpg.klimer.eu>
2020-11-25 08:15:55 +0000

    Use a slightly better (?) debounce value

--- keyboards/handwired/dactyl_lightcycle/config.h
+++ keyboards/handwired/dactyl_lightcycle/config.h
@@ -59,7 +59,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DISABLE_SPACE_CADET_ROLLOVER
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE    15
+#define DEBOUNCE    5
 
 #define PREVENT_STUCK_MODIFIERS
 
